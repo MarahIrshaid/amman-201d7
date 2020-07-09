545878627dc78d007b1cca4d4dd0f26c39c01be3
MarahIrshaid <marah.irshaid96@gmail.com>
2020-07-09 16:03:26 +0300

    q3 added

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
     var  summ = a+b ;
-    console.log( 'The sum of '+ a +' and ' + b + ' is ' + summ +'.');
+    //console.log( 'The sum of '+ a +' and ' + b + ' is ' + summ +'.');
      var answerArray= [
         summ ,
         'The sum of '+ a +' and ' + b + ' is ' + summ +'.' 
@@ -19,7 +19,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -34,7 +34,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
     var  mul = a*b ;
-    console.log( 'The product of '+ a +' and ' + b + ' is ' + mul +'.');
+   // console.log( 'The product of '+ a +' and ' + b + ' is ' + mul +'.');
      var answerArray2= [
         mul ,
         'The product of '+ a +' and ' + b + ' is ' + mul +'.' 
@@ -60,11 +60,26 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-   
+  // var  summ2 = a+b+c  ;
+  //   var product = a*b*c ;
+  var sum3= c+ sum(a,b )[0];
+  //console.log(sum3 , 'message');
+  var mul3= c* multiply(a,b)[0] ;
+  //console.log(mul3, 'my message');
+  
+     var answerArray3= [
+      sum3,
+      mul3,
+        a+ ' and ' +b+ ' and '+ c+ ' sum to '+ sum3+ '.',
+        'The product of ' + a +' and ' + b + ' and ' + c +' is '+ mul3 + '.'
+     ]
+     //console.log( 'The sum of '+ a +' and ' + b + ' and ' + c +' is ' + sum3 +'.');
+
+     return answerArray3;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
