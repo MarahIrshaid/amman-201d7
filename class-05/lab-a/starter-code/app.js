3cec98bbf2128b2a4abd82ed2f7357225c4e5096
MarahIrshaid <marah.irshaid96@gmail.com>
2020-07-09 15:25:44 +0300

    question 1 and 3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,7 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+ var  summ = a+b ;
+    console.log( 'The sum of '+ a +' and ' + b +' is ' + summ +'.');
+     var answerArray= [
+       summ,
+       'The sum of '+a+ ' and '+b+ ' is '+ summ +'.'
+     ]
+     return answerArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -48,11 +54,21 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    // // var  summ3 = a+b+c  ;
+    // // var product = a*b*c ;
+    // console.log( 'The sum of '+ a +' and ' + b + 'and ' +c+' is ' + sum3 +'.');
+    //  var answerArray= [
+    //      var sum3= sum() +c,
+    //     var multiply3= multiply()*c ,
+    //     a+ ' and ' +b+ ' and '+ c+ ' sum to '+ summ2+ '.',
+    //     'The product of ' + a +' and ' +b + ' and ' +c+' is '+ product+ '.'
+        
+    //  ]
+    //  return answerArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
