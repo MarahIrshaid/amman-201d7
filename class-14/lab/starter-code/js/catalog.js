6900d7386fa77a2692efd1ef8cbf6afb536cf8f6
dinaAlsaid <dinamalsaid@gmail.com>
2020-07-23 00:11:24 +0300

    add cart items to div

--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+
 var allProducts = [];
 
 var totalClicks = 0;
@@ -11,7 +12,7 @@ var middleImageIndex;
 var rightImageIndex;
 
 ////constructor
-function Product(name, path){
+function Product(name, path) {
   this.name = name;
   this.path = path;
 
@@ -37,8 +38,8 @@ new Product('sweep', 'img/sweep.png');
 new Product('tauntaun', 'img/tauntaun.jpg');
 new Product('unicorn meat', 'img/unicorn.jpg');
 new Product(' tale usb ', 'img/usb.gif');
-new Product('water can', 'img/water-can.jpg'); 
-new Product('wine glass', 'img/wine-glass.jpg'); 
+new Product('water can', 'img/water-can.jpg');
+new Product('wine glass', 'img/wine-glass.jpg');
 
 ////////
 // Set up an empty cart for use on this page.
@@ -54,58 +55,63 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
   console.log(selectElement.value)
-  for (var i=0 ; i< allProducts.length; i++) {
+  for (var i = 0; i < allProducts.length; i++) {
     var option = document.createElement('option');
-    option.setAttribute('value', allProducts[i].name );
-    option.textContent= allProducts[i].name;
+    option.setAttribute('value', allProducts[i].name);
+    option.textContent = allProducts[i].name;
     selectElement.appendChild(option);
 
   }
 
 }
-var selection=[];
-var button=document.querySelector("[type='submit']");
+var selection = [];
+var button = document.querySelector("[type='submit']");
 var aTag = document.createElement("a");
 aTag.setAttribute('href', 'cart.html');
-aTag.textContent='Go to Cart page';
+aTag.textContent = 'Go to Cart page';
 // console.log(button);
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
-var confirmationMessage= document.createElement('p');
+var confirmationMessage = document.createElement('p');
 var nameChoice;
+
 function handleSubmit(event) {
   event.preventDefault();
-  var inputNumber=document.querySelector("[type='number']");
- // console.log(Number(event.target.quantity.value));
-  
-  //console.log(selection);
-  
-  inputNumber.value="";
-  var div = document.getElementById("catalog");
-   confirmationMessage.textContent='product has been added to cart  ';
-   confirmationMessage.setAttribute('class' , 'conf-message'  );
-   div.appendChild(confirmationMessage);
+  var inputNumber = document.querySelector("[type='number']");
+  // console.log(Number(event.target.quantity.value));
 
-   div.appendChild(aTag);
   //console.log(selection);
-  
-  
-  // TODO: Prevent the page from reloading
+
+  var div = document.getElementById("catalog");
+  confirmationMessage.textContent = 'product has been added to cart  ';
+  confirmationMessage.setAttribute('class', 'conf-message');
+  div.appendChild(confirmationMessage);
+
+  div.appendChild(aTag);
+
 
   // Do all the things ...
+
   addSelectedItemToCart();
-  cart.saveToLocalStorage();
+  console.log(selection);
+  inputNumber.value = "";
+
+
+  // cart.saveToLocalStorage();
   updateCounter();
   updateCartPreview();
-  addSelectedItemToCart();
 }
 
+var catalogForm = document.getElementById('catalog');
+catalogForm.addEventListener('submit', handleSubmit);
 
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  selection.push(Number(event.target.quantity.value));
+  console.log(Number(event.target.quantity.value), event.target.quantity, event.target.items);
   selection.push(event.target.items.value);
+  selection.push(Number(event.target.quantity.value));
+
 
   // TODO: suss out the item picked from the select list
   // TODO: get the quantity
@@ -113,21 +119,39 @@ function addSelectedItemToCart() {
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  var sum = 0;
+  for (let i = 1; i < selection.length; i += 2) {
+    sum = selection[i] + sum;
+  }
+  console.log(sum);
+}
 
-  selection [0] = 
 
+var cart = document.getElementById("cartContents");
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
-// function updateCartPreview() {
-//   // TODO: Get the item and quantity from the form
-//   // TODO: Add a new element to the cartContents div with that information
-// }
+function updateCartPreview() {
+  // TODO: Get the item and quantity from the form
+  // TODO: Add a new element to the cartContents div with that information
+  var pro = document.createElement("a");
+  var l = selection.length - 1;
+  var deleteButton = document.createElement("button");
+  deleteButton.textContent="Delete this Item";
+
+  // deleteButton.id="delete"+selection.leng;
+
+  console.log(deleteButton.id);
+  pro.textContent = selection[l - 1] + " " + selection[l] + ", ";
+  cart.appendChild(deleteButton);
+
+  cart.appendChild(pro);
+
+
+}
 
 // Set up the "submit" event listener on the form.
 // This is the trigger for the app. When a user "submits" the form, it will
 // Call that handleSubmit method above and kick off the whole process
- catalogForm = document.getElementById('catalog');
-catalogForm.addEventListener('submit', handleSubmit);
 
 // Before anything else of value can happen, we need to fill in the select
 // drop down list in the form.
